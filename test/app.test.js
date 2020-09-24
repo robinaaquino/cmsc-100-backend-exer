c76064adc53675b6b1db762f1201c6f673076328
inah1229 <inah1229@yahoo.com>
2020-09-24 18:15:33 +0800

    test: add our first test case

---
+++ test/app.test.js
@@ -0,0 +1,28 @@
+
+const { build } = require('../app');
+
+require('tap').mochaGlobals();
+require('should');
+
+describe('For the route for root (/)', () => {
+    let app;
+
+    before(async() => {
+        //initialize backend application
+        app = await build();
+    })
+
+    it('it should return { sucess: true} and has a status code of 200 when called using GET', async () => {
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        });
+
+        const payload = response.json();
+        const { statusCode } = response;
+        const{ success } = payload;
+
+        success.should.equal(true);
+        statusCode.should.equal(200);
+    });
+});
