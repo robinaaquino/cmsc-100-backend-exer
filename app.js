299e1a2d922ea13ff2f6ba417143fbd2ce37bd91
inah1229 <inah1229@yahoo.com>
2020-09-24 18:22:53 +0800

    refactor: modularize the routing mechanism for this project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Fastify = require('fastify');
+const { routes } = require('./routes');
 
 /** 
 *
@@ -9,17 +10,7 @@ exports.build = async(opts = { logger: true, trustProxy: true}) => {
     //initalize our server using fastify
     const app = Fastify(opts)
 
-    //access http://localhost/
-    app.get('/', {
-
-        /**     
-        *handles the request for a given route which is get
-        */
-        handler: async (req) => {
-            //response in JSON
-            return{ success: true }
-        }
-    });
+    routes(app);
 
     return app;
 }
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * initialize all the routes
+ * @param {*} app 
+ */
+
+
+exports.routes = (app) => {
+    app.get('/', { //if you pass an object in a function, you're doing pass by reference, who knew that? i didnt
+
+        /**     
+        *handles the request for a given route which is get
+        */
+        handler: async (req) => {
+            //response in JSON
+            return{ success: true }
+        }
+    });
+}
