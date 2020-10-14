070058b80e72d9ff8c691cfbfca5cde136143a51
inah1229 <inah1229@yahoo.com>
2020-10-14 20:48:11 +0800

    feat: add a 404 if no existing id is given

--- routes/todo/get.js
+++ routes/todo/get.js
@@ -13,8 +13,9 @@ exports.get = app => { //arrow function which allows modification of global vari
      * This gets one todo from the database given a unique ID
      * 
      * @param {import('fastify').FastifyRequest} request
+     * @param {import('fastify').FastifyReply<Response>} response
      */
-    app.get('/todo/:id', (request) => { //since we aren't using responses?? might need to consult what this means, request allows pagination, get method will not read the payload so we use query params
+    app.get('/todo/:id', (request, response) => { //since we aren't using responses?? might need to consult what this means, request allows pagination, get method will not read the payload so we use query params
         const { params } = request; //use url to get info
         const { id } = params;
         const encoding = 'utf8';
@@ -22,6 +23,16 @@ exports.get = app => { //arrow function which allows modification of global vari
         const todos = getTodos(filename, encoding); //which is basically getting the function from ../../lib/get-todos, to read the database
     
         const index = todos.findIndex(todo => todo.id === id);
+
+        if (index < 0){ //it's -1
+            return response
+                .code(404)
+                .send({
+                    success: false,
+                    code: 'todo/not found',
+                    message: 'Todo doesn\'t exist'
+                });
+        }
 
         const data = todos[index];
 
