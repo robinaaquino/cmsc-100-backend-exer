7be32a8fc4bf582c4e84e7270f07183680862107
inah1229 <inah1229@yahoo.com>
2020-10-17 16:20:27 +0800

    feat: add connection setup for mongoose

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//setup a connection configuration to the mongodb instance
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/**
+ * This creates a connection to the database
+ * @returns { Promise }
+ */
+exports.connect = () => new Promise((resolve, rejct) => {
+    const { connection } = mongoose;
+    connection.on('error', reject);
+    connection.once('open', resolve);
+});
+
+exports.mongoose = mongoose;
