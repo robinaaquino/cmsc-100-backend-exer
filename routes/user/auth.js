a75d4b6d19f5b1ba3befcd3fcb32b1a59073d32d
inah1229 <inah1229@yahoo.com>
2021-01-11 15:14:31 +0800

    feat: implemented auth for user

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -1,47 +1,63 @@
 /**
  * Prehandler Auth Module
  * - Should return unauthorized 401 if no token or a wrong token is given
-- Should process token from authorization header
+- Should process token from authorization header **finished alongside exer
 - Should process token from the cookie session
  */
 
 const { definitions } = require('../../definitions');
-const { SuccessResponse } = definitions;
+const { AuthQuery, SuccessResponse } = definitions;
 
 /**
- * this is the route for checking if a user is authenticated
- * @param {*} app 
+ * this is the route for checking if the user is authenticated
+ *
+ * @param {*} app
  */
-
 exports.auth = app => {
-    app.get('/auth', {
-        schema: {
-            description: 'Check authentication of a user',
-            tags: ['User'],
-            summary: 'Check authentication of a user',
-            response: {
-                200: SuccessResponse //using response we can filter out what we want to show on our response
-            },
-            security: [
-                {
-                    bearer: []
-                }
-            ]
-
-        },
-
-        preHandler: app.auth([
-            app.verifyJWT
-        ]),
-
-        /**
-         * handles the request for a given route
-         * 
-         */
-        handler: async () => {
-            return {
-                success: true
-            }
+  app.get('/auth', {
+    schema: {
+      description: 'Check authentication of a user',
+      tags: ['User'],
+      summary: 'Check authentication of a user',
+      query: AuthQuery,
+      response: {
+        200: SuccessResponse
+      },
+      security: [
+        {
+          bearer: []
         }
-    })
+      ]
+    },
+    preHandler: app.auth([
+      app.verifyJWT
+    ]),
+    /**
+     * handles the request for a given route
+     *
+     */
+    handler: async (request, response) => {
+        const { query, session, headers } = request;
+        const { tokenQuery } = query;
+        const { token: cookieToken } = session;
+        const { authorization } = headers;
+
+        let authorizationToken;
+
+        if(authorization){
+            [,authorizationToken] = authorization.split('Bearer ');
+        }
+
+        const token = cookieToken || authorizationToken
+
+        if(!tokenQuery || tokenQuery !== token ){
+            return response
+                .unauthorized('user/unauthorized');
+        }
+
+        return {
+            success: true
+        }
+    }
+  })
 };
