97a178b0484a237bf63f0f9e17cfba312273bc9c
inah1229 <inah1229@yahoo.com>
2021-01-12 21:53:27 +0800

    feat: implement logout specifications

--- routes/user/logout.js
+++ routes/user/logout.js
@@ -1,13 +1,13 @@
 /**
  * Logout Module
- * - Should return unauthorized if no token or wrong token is given
-- Should add token to forbidden list to disallow the reuse of the token
-- Should remove the token in the session cookie
+ * - Should return unauthorized if no token or wrong token is given **finished
+- Should add token to forbidden list to disallow the reuse of the token **finished
+- Should remove the token in the session cookie**finished along with exer
  */
 
 const { DiscardedToken } = require('../../db');
 const { definitions } = require('../../definitions');
-const { SuccessResponse } = definitions;
+const { AuthQuery, SuccessResponse } = definitions;
 /**
  * this is the route for logging out a user
  * @param {*} app 
@@ -19,6 +19,7 @@ exports.logout = app => {
             description: 'Logs out a user',
             tags: ['User'],
             summary: 'Logs out a user',
+            query: AuthQuery,
             response: {
                 200: SuccessResponse //using response we can filter out what we want to show on our response
             },
@@ -41,11 +42,18 @@ exports.logout = app => {
          * @param {import('fastify').FastifyReply<Response>} response
          */
         handler: async (request, response) => {
-            const { token, user } = request;
+            const { user, query } = request;
             const { username } = user;
+            const { tokenQuery } = query;
+
+            if(tokenQuery != request.session.token || !tokenQuery){
+                return response
+                    .unauthorized('user/unauthorized')
+            }
+
             const data = new DiscardedToken({
                 username,
-                token
+                token: tokenQuery
             });
 
             await data.save();
