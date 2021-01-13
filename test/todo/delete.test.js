a07f06c4c90efa1220d3f6cbd01df08cc9f29cc0
inah1229 <inah1229@yahoo.com>
2021-01-13 10:06:49 +0800

    test: implemented test for delete todo

--- test/todo/delete.test.js
+++ test/todo/delete.test.js
@@ -1,22 +1,21 @@
 const { mongoose, Todo, User } = require('../../db');
-const { delay } = require('../../lib/delay');
 const { build } = require('../../app');
 const should = require('should');
 require('tap').mochaGlobals();
-
 
 describe('For the route for deleting one todo DELETE: (/todo/:id)', () => {
     let app;
     let authorization;
     const ids = [];
 
-    before(async() => {
-        //initialize backend application
+    before(async () => {
         app = await build();
 
         const payload = {
             username: 'testuser1',
-            password: 'password1234567890'
+            password: 'passwordpassword',
+            firstName: 'user',
+            lastName: 'user',
         }
 
         await app.inject({
@@ -29,13 +28,13 @@ describe('For the route for deleting one todo DELETE: (/todo/:id)', () => {
             method: 'POST',
             url: '/login',
             payload
-        });
+        })
 
         const { data: token } = response.json();
-
+        
         authorization = `Bearer ${token}`;
 
-        for(let i = 0; i<1; i++){
+        for(let i = 0; i < 1; i++){
             const response = await app.inject({
                 method: 'POST',
                 url: '/todo',
@@ -43,78 +42,66 @@ describe('For the route for deleting one todo DELETE: (/todo/:id)', () => {
                     authorization
                 },
                 payload: {
-                    text: `Todo ${i}`, //usage of backticks for addresses
+                    text: `Todo ${i}`,
                     isDone: false
                 }
             });
 
-
+            const data = await Todo.findOne().sort({ dateCreated: -1 }).limit(1).exec();
             const payload = response.json();
-
-            console.log(payload);
-            const { data } = payload;
-            const { id } = data; //we need ids
-
-            ids.push(id); //allows the deletion later on
-            await delay(1000);
+            const { id } = data;
+            ids.push(id);
         }
-        //best case is to use a control data set  
-    })
+        
+    });
 
     after(async () => {
-        //clean up the database
-        for (const id of ids){
-            await Todo.findOneAndDelete({ id });
+        for(const id of ids){
+            await Todo.findOneAndDelete({ id, username: 'testuser1' });
         }
 
         await User.findOneAndDelete({ username: 'testuser1' });
 
         await mongoose.connection.close();
-        
     });
 
     //happy path
-    it('it should return { success: true} and has a status code of 200 when called using DELETE', async () => {
+    it('it should return { success: true } and has a status code of 200 when called using DELETE', async () => {
+        const id = ids[0];
+
         const response = await app.inject({
             method: 'DELETE',
             url: `/todo/${ids[0]}`,
             headers: {
                 authorization
-            },
+            }
         });
 
         const payload = response.json();
-
-        console.log(payload);
-        console.log(response.statusCode);
-
         const { statusCode } = response;
-        const { success, data } = payload;
-        const id = ids[0];
-
+        const { success } = payload;
+        
         success.should.equal(true);
         statusCode.should.equal(200);
 
         const todo = await Todo
             .findOne({ id })
             .exec();
-        should.not.exists(todo);
-    });
 
-    it('it should return { success: false, message: error message} and has a status code of 404 when called using DELETE and the id of the todo is non-existing', async () => {
+        should.not.exists(todo);
+    })
+
+    //non-happy path
+    it('it should return { success: false, message: error message } and has a status code of 404 when called using DELETE and the id of the todo is non-existing', async() => {
         const response = await app.inject({
             method: 'DELETE',
-            url: `/todo/non-existing-id`,
+            url: '/todo/non-existing-id',
             headers: {
                 authorization
-            },
+            }
         });
 
         const payload = response.json();
-
-        console.log(payload);
-        console.log(response.statusCode);
-
         const { statusCode } = response;
         const { success, code, message } = payload;
 
@@ -123,6 +110,5 @@ describe('For the route for deleting one todo DELETE: (/todo/:id)', () => {
 
         should.exists(code);
         should.exists(message);
-
-    });
-});
+    })
+})
