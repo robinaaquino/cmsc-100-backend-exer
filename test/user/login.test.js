1ae8553e80d2eea21acc825639dd1fde1ee8a203
inah1229 <inah1229@yahoo.com>
2021-01-13 18:40:48 +0800

    test: implemented paths for login user

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -3,83 +3,75 @@ const { build } = require('../../app');
 const should = require('should');
 require('tap').mochaGlobals();
 
-
-describe('For the route for creating a user POST: (/user)', () => {
+describe('For the route for creating a user POST: (/user', () => {
     let app;
     const usernames = [];
-
-
-    before(async() => {
-        //initialize backend application
+    
+    before(async () => {
         app = await build();
-
-        for(let i = 0; i<1; i++){
+        for(let i = 0; i < 1; i++){
             const response = await app.inject({
                 method: 'POST',
                 url: '/user',
                 payload: {
-                    username: `user ${i}`, //usage of backticks for addresses
-                    password: 'password1234567890'
+                    username: `user${i}`,
+                    firstName: 'test',
+                    lastName: 'test',
+                    password: 'passwordpassword'
                 }
             });
 
-            const payload = response.json();
-            const { data } = payload;
-            const { username } = data; //we need ids
+            const data = await User.findOne().sort({ dateCreated: -1 }).limit(1).exec();
+            const { username } = data;
 
-            usernames.push(username); //allows the deletion later on
+            usernames.push(username);
+
         }
-
-        //best case is to use a control data set
-    })
+    });
 
     after(async () => {
-        //clean up the database
-        for (const username of usernames){
+        for(const username of usernames){
             await User.findOneAndDelete({ username });
         }
 
         await mongoose.connection.close();
     });
 
-    //happy path
-    it('it should return { success: true, data: (new user object)} and has a status code of 200 when called using POST', async () => {
+    it('it should return { success: true, data: jwt } and has a status code of 200 when called using POST', async() => {
         const response = await app.inject({
             method: 'POST',
             url: '/login',
             payload: {
                 username: usernames[0],
-                password: 'password1234567890'
+                password: 'passwordpassword'
             }
-        });
+        })
 
         const payload = response.json();
         const { statusCode } = response;
         const { success, data } = payload;
-        // const { username } = data;
-
+        
         success.should.equal(true);
         statusCode.should.equal(200);
-        // username.should.equal('user01');
-    });
+        should.exist(data);
+    })
 
-    //non-happy path
-    it ('should return {success: false, message: error message)} and has a status code of 401 when called using POST and we use a different password', async () => {
+    //non-happy
+    it('it should return { success: true, message: error message } and has a status code of 401 when called using POST and there\'s no username in payload', async() => {
         const response = await app.inject({
             method: 'POST',
             url: '/login',
             payload: {
-                username: usernames[0],
-                password: 'password123'
+                password: 'passwordpassword'
             }
-        });
+        })
 
         const payload = response.json();
         const { statusCode } = response;
         const { success, message } = payload;
 
-        statusCode.should.equal(401);
+        statusCode.should.equal(400);
         success.should.equal(false);
         should.exist(message);
     })
-});
+})
