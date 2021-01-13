6ca9b31537bdb63700e9c3b62618c7ef54799c88
inah1229 <inah1229@yahoo.com>
2021-01-13 18:47:58 +0800

    chore: changed required prop for GetOneUserRes

--- definitions/index.js
+++ definitions/index.js
@@ -356,7 +356,7 @@ const UpdateOneUserResponse = {
 const GetOneUserResponse = {
     type: 'object',
     description: 'Returns a user',
-    required: ['success', 'data'],
+    required: ['success'],
     properties: {
         success: SuccessData,
         data: UserFullData
