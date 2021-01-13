8e71fd098d03e96de1b8ea7cbd0a8794081bff7e
inah1229 <inah1229@yahoo.com>
2021-01-13 18:41:47 +0800

    test: implemented paths for get todo

--- test/todo/get.test.js
+++ test/todo/get.test.js
@@ -1,29 +1,28 @@
 const { mongoose, Todo, User } = require('../../db');
-const { delay } = require('../../lib/delay');
 const { build } = require('../../app');
 const should = require('should');
 require('tap').mochaGlobals();
-
 
 describe('For the route for getting one todo GET: (/todo/:id)', () => {
     let app;
     let authorization = '';
     const ids = [];
 
-    before(async() => {
-        //initialize backend application
+    before(async () => {
         app = await build();
 
         const payload = {
             username: 'testuser3',
-            password: 'password1234567890'
+            firstName: 'test',
+            lastName: 'test',
+            password: 'passwordpassword'
         }
 
         await app.inject({
             method: 'POST',
             url: '/user',
             payload
-        });
+        })
 
         const response = await app.inject({
             method: 'POST',
@@ -35,92 +34,72 @@ describe('For the route for getting one todo GET: (/todo/:id)', () => {
 
         authorization = `Bearer ${token}`;
 
-        for(let i = 0; i<1; i++){
-            const response = await app.inject({
+        for(let i = 0; i < 1; i ++){
+            const response = await app.inject ({
                 method: 'POST',
                 url: '/todo',
                 headers: {
                     authorization
                 },
                 payload: {
-                    text: `Todo ${i}`, //usage of backticks for addresses
+                    text: `Todo ${i}`,
                     isDone: false
                 }
-            });
+            })
 
+            const data = await Todo.findOne().sort({ dateCreated: -1 }).limit(1).exec(); 
+            const { id } = data;
 
-            const payload = response.json();
-
-            console.log(payload);
-            const { data } = payload;
-            const { id } = data; //we need ids
-
-            ids.push(id); //allows the deletion later on
-            await delay(1000);
+            ids.push(id);
         }
+    });
 
-        //best case is to use a control data set
-
-
-        
-    })
-
-    after(async () => {
-        //clean up the database
-        for (const id of ids){
+    after(async () =>{
+        for(const id of ids) {
             await Todo.findOneAndDelete({ id });
         }
 
         await User.findOneAndDelete({ username: 'testuser3' });
 
         await mongoose.connection.close();
-    });
+    })
 
+    it('it should return { success: true, data: todo } and has a status code of 200 when called using GET', async () => {
+        const idFilter = ids[0];
 
-    it('it should return { success: true, data: todo} and has a status code of 200 when called using GET', async () => {
         const response = await app.inject({
             method: 'GET',
             url: `/todo/${ids[0]}`,
             headers: {
                 authorization
-            },
+            }
         });
 
         const payload = response.json();
-
-        console.log(payload);
-        console.log(response.statusCode);
-
         const { statusCode } = response;
         const { success, data } = payload;
-        const { text,id,isDone } = data;
+        const { username, text, isDone } = data;
 
-        success.should.equal(true);
+        const todo = await Todo.findOne({ id: idFilter }).exec();
+
         statusCode.should.equal(200);
-
-        const todo = await Todo
-            .findOne({ id })
-            .exec();
-
+        success.should.equal(true);
+        username.should.equal(todo.username);
         text.should.equal(todo.text);
         isDone.should.equal(todo.isDone);
-        id.should.equal(todo.id);
+
     });
 
-    it('it should return { success: false, message: error message} and has a status code of 404 when called using GET and the id of the todo is non-existing', async () => {
+    it('it should return { succes: false, message: error message } and has a status code of 404 when called using GET and the id of the todo is non-existing', async () => {
         const response = await app.inject({
             method: 'GET',
             url: `/todo/non-existing-id`,
             headers: {
                 authorization
-            },
+            }
         });
 
         const payload = response.json();
-
-        console.log(payload);
-        console.log(response.statusCode);
-
         const { statusCode } = response;
         const { success, code, message } = payload;
 
@@ -129,6 +108,5 @@ describe('For the route for getting one todo GET: (/todo/:id)', () => {
 
         should.exists(code);
         should.exists(message);
-
-    });
-});
+    })
+})
