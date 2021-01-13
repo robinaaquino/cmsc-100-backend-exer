7d022e6e11b49d28f780035950d20a6476e6b826
inah1229 <inah1229@yahoo.com>
2021-01-13 10:05:37 +0800

    test: implemented test for create todo

--- test/todo/create.test.js
+++ test/todo/create.test.js
@@ -3,19 +3,18 @@ const { build } = require('../../app');
 const should = require('should');
 require('tap').mochaGlobals();
 
-
 describe('For the route for creating a todo POST: (/todo)', () => {
     let app;
     let authorization = '';
     const ids = [];
 
-
-    before(async() => {
-        //initialize backend application
+    before(async () => {
         app = await build();
         const payload = {
-            username: 'testuser', //tests run parallel to each other so need to differentiate between testusers
-            password: 'password1234567890'
+            username: 'testuser',
+            firstName: 'user',
+            lastName: 'user',
+            password: 'passwordpassword'
         }
 
         await app.inject({
@@ -32,11 +31,12 @@ describe('For the route for creating a todo POST: (/todo)', () => {
 
         const { data: token } = response.json();
 
-        authorization = `Bearer ${token}`;
-    })
+        authorization = `Bearer ${ token }`;
+
+
+    });
 
     after(async () => {
-        //clean up the database
         for (const id of ids){
             await Todo.findOneAndDelete({ id });
         }
@@ -47,7 +47,7 @@ describe('For the route for creating a todo POST: (/todo)', () => {
     });
 
     //happy path
-    it('it should return { success: true, data: (new todo object)} and has a status code of 200 when called using POST', async () => {
+    it('it should return { success: true } and has a status code of 200 when called using POST', async() => {
         const response = await app.inject({
             method: 'POST',
             url: '/todo',
@@ -62,31 +62,20 @@ describe('For the route for creating a todo POST: (/todo)', () => {
 
         const payload = response.json();
         const { statusCode } = response;
-        const { success, data } = payload;
-        const { text, isDone, id } = data;
+        const { success } = payload;
 
         success.should.equal(true);
         statusCode.should.equal(200);
-        text.should.equal('This is a todo');
-        isDone.should.equal(false);
 
-        const { 
-            text: textDatabase, 
-            isDone: isDoneDatabase 
-        } = await Todo
-            .findOne({ id })
-            .exec();
+        const data = await Todo.findOne().sort({ dateCreated: -1 }).limit(1).exec(); //finding the most recent addition to the document
 
-        text.should.equal(textDatabase);
-        isDone.should.equal(isDoneDatabase);
+        const { id } = data;
 
-        //add the id in the ids array for cleaning
         ids.push(id);
-
     });
 
-    //another happy path but a different way of sending data
-    it ('should return {success: true, data: (new todo object)} and has a status code of 200 when called using POST even if we don\'t provide the isDne property. Default of isDone should still be false', async () => {
+    //non-happy path
+    it('it should return { success: true } and has a status code of 200 when called using POST even if we don\'t provide the done property. Default of done should still be false', async() => {
         const response = await app.inject({
             method: 'POST',
             url: '/todo',
@@ -97,69 +86,18 @@ describe('For the route for creating a todo POST: (/todo)', () => {
                 text: 'This is a todo 2'
             }
         });
-        
+
         const payload = response.json();
         const { statusCode } = response;
-        const { success, data } = payload;
-        const { text, isDone, id } = data;
-
+        const { success } = payload;
+        
         success.should.equal(true);
         statusCode.should.equal(200);
-        text.should.equal('This is a todo 2');
-        isDone.should.equal(false);
 
-        const { 
-            text: textDatabase, 
-            isDone: isDoneDatabase 
-        } = await Todo
-            .findOne({ id })
-            .exec();
+        const newData = await Todo.findOne().sort({ dateCreated: -1 }).limit(1).exec(); //finding the most recent addition to the document
 
-        text.should.equal(textDatabase);
-        isDone.should.equal(isDoneDatabase);
+        const { id } = newData
 
-        //add the id in the ids array for cleaning
         ids.push(id);
-    });
-
-    //non-happy path
-    it ('should return {success: false, message: error message)} and has a status code of 400 when called using POST and there is no text', async () => {
-        const response = await app.inject({
-            method: 'POST',
-            url: '/todo',
-            headers: {
-                authorization
-            },
-            payload: {
-                isDone: true
-            }
-        });
-
-        const payload = response.json();
-        const { statusCode } = response;
-        const { success, message } = payload;
-
-        statusCode.should.equal(400);
-        success.should.equal(false);
-        should.exist(message);
-    })
-
-    //another non-happy path
-    it ('should return {success: false, message: error message)} and has a status code of 400 when called using POST and there is no payload', async () => {
-        const response = await app.inject({
-            method: 'POST',
-            url: '/todo',
-            headers: {
-                authorization
-            },
-        });
-
-        const payload = response.json();
-        const { statusCode } = response;
-        const { success, message } = payload;
-
-        statusCode.should.equal(400);
-        // success.should.equal(false);
-        should.exist(message);
     })
 });
